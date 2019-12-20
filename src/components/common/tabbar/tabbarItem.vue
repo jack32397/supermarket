41570e72a070dc7dbaf65674bc2f9f69867e7fe8
jack32397 <1746669871@qq.com>
2019-12-20 15:48:40 +0800

    1

---
+++ src/components/common/tabbar/tabbarItem.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="tab-bar-item" @click="clickItem">
+    <div v-if="!isActive">
+      <slot name="item-icon"></slot>
+    </div>
+    <div v-else>
+      <slot name="item-icon-active"></slot>
+    </div>
+    <div :class="{active:isActive}">
+      <slot name="item-text"></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "tabbarItem",
+  props: {
+    path: String
+  },
+  methods: {
+    clickItem() {
+      if (this.$route.path !== this.path) {
+        this.$router.replace(this.path);
+      }
+    }
+  },
+  computed: {
+    isActive() {
+      return this.$route.path == this.path;
+    }
+  }
+};
+</script>
+
+<style scoped>
+.tab-bar-item {
+  flex: 1;
+  text-align: center;
+  height: 49px;
+}
+.tab-bar-item img {
+  height: 24px;
+  width: 24px;
+  vertical-align: middle;
+  margin-top: 3px;
+  margin-bottom: 2px;
+}
+.active {
+  color: red;
+}
+</style>
---
+++ src/components/content/mainTabbar/MainTabBar.vue
@@ -0,0 +1,37 @@
+<template>
+  <tabbar>
+    <tabbarItem path="/home">
+      <img slot="item-icon" src="~assets/img/tabbar/home.svg" />
+      <img slot="item-icon-active" src="~assets/img/tabbar/home_active.svg" />
+
+      <div slot="item-text">首页</div>
+    </tabbarItem>
+    <tabbarItem path="/category">
+      <img slot="item-icon" src="~assets/img/tabbar/category.svg" />
+      <img slot="item-icon-active" src="~assets/img/tabbar/category_active.svg" />
+      <div slot="item-text">分类</div>
+    </tabbarItem>
+    <tabbarItem path="/cart">
+      <img slot="item-icon" src="~assets/img/tabbar/cart.svg" />
+      <img slot="item-icon-active" src="~assets/img/tabbar/cart_active.svg" />
+      <div slot="item-text">购物车</div>
+    </tabbarItem>
+    <tabbarItem path="/profile">
+      <img slot="item-icon" src="~assets/img/tabbar/profile.svg" />
+      <img slot="item-icon-active" src="~assets/img/tabbar/profile_active.svg" />
+      <div slot="item-text">我的</div>
+    </tabbarItem>
+  </tabbar>
+</template>
+<script>
+import tabbar from '../../common/tabbar/tabbar'
+import tabbarItem from '../../common/tabbar/tabbarItem'
+
+export default {
+  name: 'MainTabBar',
+  components: {
+    tabbar,
+    tabbarItem
+  }
+}
+</script>
